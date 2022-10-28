9e18bbab74d29e1509db4b166da1a3c2f630f87a
Justin Purdie <jpurdie@users.noreply.github.com>
2022-10-28 14:02:09 -0700

    Upgrade testing library

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,7 +1,15 @@
-function Button() {
-  return (
-    <button type="button" className="btn btn-primary">Primary</button>
-  );
+interface Props {
+  type: string;
+  text: string;
+  onClick: () => void;
 }
+
+const Button: React.FC<Props> = ({ type, text, onClick }) => {
+  return (
+    <button type='button' onClick={onClick} className={`btn btn-${type ? type : 'primary'}`}>
+      {text}
+    </button>
+  );
+};
 
 export default Button;
--- src/shared/components/Button/button.test.tsx
+++ src/shared/components/Button/button.test.tsx
@@ -5,8 +5,16 @@ import { render, screen, userEvent } from '../../../utils/test-utils';
 import Button from './Button';
 
 describe('Input', async () => {
-  test('should render the button', () => {
-    render(<Button />);
-    expect(screen.getByText('Primary')).toBeInTheDocument();
+  test("should render the button with text 'foo'", () => {
+    render(<Button text='foo' onClick={() => {}} type='primary' />);
+    const el = screen.getByText('foo');
+    expect(el).toBeInTheDocument();
+  });
+
+  test("should render the button with class 'primary'", () => {
+    render(<Button text='foo' onClick={() => {}} type='primary' />);
+    const el = screen.getByText('foo');
+    const actual = el.classList.contains('btn-primary');
+    expect(actual).toBe(true);
   });
 });
