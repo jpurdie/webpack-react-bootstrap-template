691e6c8f1450e919120a3d0cfde5f6d17f9b2a0e
Justin Purdie <justindpurdie@gmail.com>
2022-11-03 19:57:01 -0700

    Adding webpack dev server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,12 +1,17 @@
 const paths = require('./paths');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   // Set the mode to development or production
   mode: 'development',
   // Control how source maps are generated
   devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+  },
   module: {
     rules: [],
   },
